958abee28586b0d42728b8d07c002dca9ef0a70d
HlebHlev <hlebhlevhlam@yandex.ru>
2023-01-05 17:21:24 +0700

    awake and alive

--- server_components/bot_components/products.js
+++ server_components/bot_components/products.js
@@ -244,7 +244,7 @@ const deleteProduct = (bot, chatId, id) => {
                                 console.error(err.message);
                                 return;
                             }
-                            console.log(currentList)
+                            console.log(currentList.find(el => el.date === id))
                             // bot.deleteMessage(chatId, currentList.find(el => el.id === id))
                             //     .then(() => {
                             //         bot.sendMessage(chatId, 'Product deleted successfully!', {
