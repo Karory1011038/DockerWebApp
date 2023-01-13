18060ec70f66949d36da8f2105c1b48bbed310e6
HlebHlev <hlebhlevhlam@yandex.ru>
2023-01-14 01:38:12 +0700

    changes

--- client/src/components/products/ProductOrderList.vue
+++ client/src/components/products/ProductOrderList.vue
@@ -1,6 +1,6 @@
 <template>
     <div v-if="products.length">
-        <order-card v-for="(item,index) in products" :style="index === 0?'':{'margin-top':'10px'}" :product="product">
+        <order-card v-for="(product,index) in products" :style="index === 0?'':{'margin-top':'10px'}" :product="product">
         </order-card>
     </div>
     <div v-else>
