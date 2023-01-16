e40d5a587108460ab446c7b48bb6187aaf11a0ac
HlebHlev <hlebhlevhlam@yandex.ru>
2023-01-16 23:13:06 +0700

    add desk af 1

--- client/src/stores/cart.js
+++ client/src/stores/cart.js
@@ -1,26 +1,36 @@
-import {ref, computed} from 'vue'
-import {defineStore} from 'pinia'
-
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
 
 export const useCartStore = defineStore('cart', {
-    state: () => ({
-        cart: ref(JSON.parse(localStorage.getItem('cart')) || {} )
-    }),
-    getters: {
-        getCart: (state) => state.cart,
+    state: () => {
+        let cartData = {}
+        try {
+            cartData = JSON.parse(localStorage.getItem('cart')) || {}
+        } catch (e) {
+            console.error(`Error loading cart data from localStorage: ${e}`)
+        }
+        return {
+            cart: ref(cartData)
+        }
     },
-    actions: {
-        addItem(item) {
-            this.cart[item.id] = this.cart[item.id] ? this.cart[item.id] + 1 : 1
-            localStorage.setItem('cart', JSON.stringify(this.cart));
+    mutations: {
+        addItem(state, item) {
+            state.cart.value[item.id] = state.cart.value[item.id] ? state.cart.value[item.id] + 1 : 1
+            localStorage.setItem('cart', JSON.stringify(state.cart.value))
         },
-        clearCart() {
-            this.cart = {}
+        clearCart(state) {
+            state.cart.value = {}
             localStorage.removeItem('cart')
         },
-        deleteItem(item) {
-            this.cart[item.id] = this.cart[item.id] ? this.cart[item.id] - 1 : 0
-            localStorage.setItem('cart', JSON.stringify(this.cart));
+        deleteItem(state, item) {
+            if (!state.cart.value[item.id]) return
+            state.cart.value[item.id] = state.cart.value[item.id] > 1 ? state.cart.value[item.id] - 1 : undefined
+            localStorage.setItem('cart', JSON.stringify(state.cart.value))
         }
+    },
+    getters: {
+        cartCount: (state) => Object.values(state.cart.value).reduce((count, itemCount) => count + itemCount, 0),
+        isInCart: (state) => (itemId) => !!state.cart.value[itemId],
+        getCart: (state) => state.cart.value,
     },
 })
