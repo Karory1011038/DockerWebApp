a42790d9c68322e1b818d05986684cedd678181d
HlebHlev <hlebhlevhlam@yandex.ru>
2023-01-25 03:27:42 +0700

    create new front again

--- client/src/components/opders/createOrderSteps/FirstStep.vue
+++ client/src/components/opders/createOrderSteps/FirstStep.vue
@@ -45,5 +45,38 @@ onMounted(() => {
 </script>
 
 <style scoped>
+.order-title {
+    font-weight: 700;
+    font-size: 26px;
+    line-height: 28px;
+}
 
+.order-number{
+    font-size: 16px;
+    line-height: 16px;
+    font-weight: 500;
+}
+
+.edit-button {
+    margin:  auto 0;
+    font-weight: 600;
+    font-size: 18px;
+    line-height: 19px;
+    background-color: black;
+    border-radius: 30px;
+    text-align: center;
+    padding: 15px 25px;
+}
+
+.total-label {
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 19px;
+}
+
+.total-price {
+    font-weight: 700;
+    font-size: 16px;
+    line-height: 21px;
+}
 </style>
